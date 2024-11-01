15ceb0e7cbe4a3ed1e607280711c98e65ec106ec
moshpitchuu <v.hardy@mcaabhm.org>
2024-11-01 12:46:56 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. a a a . a a a a . a a a . 
+a a a a f c c c c f a a a a 
+a a a f b c c c c b f a a a 
+a a f c 3 c c c c 3 c f a a 
+. f 3 3 c c c c c c 3 3 f . 
+. f c c c c b b c c c c f . 
+. f a c c b b b b c c a f . 
+. f f a b f b b f b a f f . 
+. f f b 1 f b b f 1 b f f . 
+. . f f b b b b b b f f . . 
+. . e f e a a a a e f e . . 
+. e b f b b b b b b f b e . 
+. b d f 1 1 1 1 1 1 c d b . 
+. b b f f 1 1 1 1 f f b b . 
+. . . . b b f f b b . . . . 
+. . . . f f . . f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
